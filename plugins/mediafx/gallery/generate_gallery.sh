19362502e156044e29feffb88e7a652ea3e95f74
Daniel McFeeters <fiforms@gmail.com>
2026-02-16 21:29:11 -0500

    Added snowflake preset

--- plugins/mediafx/gallery/generate_gallery.sh
+++ plugins/mediafx/gallery/generate_gallery.sh
@@ -15,3 +15,7 @@ ffmpeg -i waves_subtle.mp4 -ss 00:00:10 -vframes 1 waves_subtle.jpg
 rm -f flames_candles.mp4 flames_candles.jpg
 ../../../bin/effectgenerator --width 1920 --height 1080 --fps 30 --fade 0 --crf 23 --duration 20 --background-image demo_base.jpg --warmup 5 --effect flame --preset candle --sources '897,795,1.2;175,880,1.0;1740,980,1.6' --effect loopfade --output - | ffmpeg -f rawvideo -pix_fmt rgb24 -s 1920x1080 -r 30 -i - -vf "scale=960:540" -c:v libx264 -crf 32 -movflags faststart flames_candles.mp4
 ffmpeg -i flames_candles.mp4 -ss 00:00:10 -vframes 1 flames_candles.jpg
+
+rm -f snowflake.mp4 snowflake.jpg
+../../../bin/effectgenerator --width 1920 --height 1080 --fps 30 --fade 2 --crf 23 --duration 30 --background-image demo_base.jpg --warmup 0 --max-fade 1 --effect snowflake --brightness 0.8 --flakes 250 --size-var 1.6 --motion-x -0.6 --motion-y 1.9 --size-bias 4 --size 2 --min-size 0.2 --randomness 1.5 --softness 3 --output - | ffmpeg -f rawvideo -pix_fmt rgb24 -s 1920x1080 -r 30 -i - -vf "scale=960:540" -c:v libx264 -crf 32 -movflags faststart snowflake.mp4
+ffmpeg -i snowflake.mp4 -ss 00:00:10 -vframes 1 snowflake.jpg
